dd62ab796038bf165d5b68089f12013773cefb03
José PC <joseraimundoalves@hotmail.com>
2023-05-23 10:43:31 -0300

    Finalizando menuPrincipal incrmeental

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,10 +1,78 @@
+import java.util.Scanner;
+
 public class Aplicacao {
-    Scanner in = new Scanner(System.in);
-    public static void main(String[] args){
-        System.out.println("Testando console");
-        in.nextLine();
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            menuPrincipal();
+        }
     }
-    
+
+    private static void menuPrincipal() {
+        System.out.println("Defina a opção: \na) cadastrar imóvel \nb) cadastrar propietário");
+        String opcao = in.nextLine();
+        if (opcao.equals("a")) {
+            System.out.println("Qual o nome da rua? \n");
+            String rua = in.nextLine();
+            System.out.println("Qual o tipo de imóvel? \n");
+            String tipo = in.nextLine();
+            System.out.println("Qual a utilização? \n");
+            String utilizacao = in.nextLine();
+            System.out.println("Qual a cidade? \n");
+            String cidade = in.nextLine();
+            System.out.println("Qual o estado? \n");
+            String estado = in.nextLine();
+            System.out.println("Qual o valor do IPTU? \n");
+            Integer iptu = in.nextInt();
+            System.out.println("Qual o CEP? \n");
+            Integer cep = in.nextInt();
+            System.out.println("Qual o número da casa? \n");
+            Integer numero = in.nextInt();
+            cadastrarImovel(iptu, tipo, utilizacao, rua, numero, cep, cidade, estado);
+        } else if (opcao.equals("b")) {
+            System.out.println("Qual o nome do propietario? \n");
+            String nome = in.nextLine();
+            System.out.println("Qual o cpf do propietario? \n");
+            String cpf = in.nextLine();
+            System.out.println("Qual o nome da rua de residência propietario? \n");
+            String rua = in.nextLine();
+            System.out.println("Qual a cidade de residência do propietario? \n");
+            String cidade = in.nextLine();
+            System.out.println("Qual o estado de residência do propietario? \n");
+            String estado = in.nextLine();
+            System.out.println("Qual o RG do propietario? \n");
+            Integer rg = in.nextInt();
+            System.out.println("Qual o numero da casa do propietario? \n");
+            Integer numero = in.nextInt();
+            System.out.println("Qual o cep de residência do propietario? \n");
+            Integer cep = in.nextInt();
+
+            cadastrarPropietario(nome, cpf, rg, rua, cep, cidade, estado, numero);
+            
+        }
+
+    }
+
+    private static void cadastrarPropietario(String nome, String cpf, int rg, String rua, int cep, String cidade,
+            String estado, int numero) {
+        Propietario propietario = new Propietario(nome, cpf, rg, rua, cep, cidade, estado, numero);
+        listaPropietario(propietario);
+    }
+
+    private static void listaPropietario(Propietario propietario) {
+        System.out.println(propietario.toString());
+    }
+
+    private static void cadastrarImovel(int iptu, String tipo, String utilizacao, String rua, int numero, int cep,
+            String cidade, String estado) {
+        Imovel imovel = new Imovel(iptu, tipo, utilizacao, rua, numero, cep, cidade, estado);
+        listaImovel(imovel);
+    }
+
+    private static void listaImovel(Imovel imovel) {
+        System.out.println(imovel.toString());
+    }
+
 }
-
-
--- Propietario.java
+++ Propietario.java
@@ -1,22 +1,32 @@
 import java.util.ArrayList;
 
 public class Propietario {
-    private int cpf, rg;
+    private int rg;
+    private String nome, cpf;
     private Endereco endereco;
     private ArrayList<Imovel> imoveis_do_propietario = new ArrayList<Imovel>();
-    public Propietario(int cpf,int rg, String rua, int cep,String cidade, String estado,int numero , Imovel imovel) {
+
+    public Propietario(String cpf, int rg, String rua, int cep, String cidade, String estado, int numero,
+            Imovel imovel) {
         this.setCpf(cpf);
         this.setRg(rg);
-        this.endereco = new Endereco(rua,numero, cep, cidade, estado );//COMPOSIÇÃO FORTE!
+        this.endereco = new Endereco(rua, numero, cep, cidade, estado);// COMPOSIÇÃO FORTE!
         this.imoveis_do_propietario.add(imovel);
     }
 
-    
-    public int getCpf() {
+    public Propietario(String nome, String cpf, int rg, String rua, int cep, String cidade, String estado, int numero) {
+        this.nome = nome;
+        this.setCpf(cpf);
+        this.setRg(rg);
+        this.endereco = new Endereco(rua, numero, cep, cidade, estado);// COMPOSIÇÃO FORTE!
+
+    }
+
+    public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(int cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
@@ -27,22 +37,26 @@ public class Propietario {
     public void setRg(int rg) {
         this.rg = rg;
     }
-    
+
     public void adicionaImovel(Imovel imovel) {
-    	this.imoveis_do_propietario.add(imovel);
-    }
-    
-    public void listaImoveis(){
-    	for(int i=0; i<this.imoveis_do_propietario.size(); i++) {
-    		System.out.println(this.imoveis_do_propietario.get(i).toString()); 
-    	}
-    }
-    
-    public void listaImoveisPorTipo(String tipo){
-    	for(int i=0; i<this.imoveis_do_propietario.size(); i++) {
-    		if(this.imoveis_do_propietario.get(i).getTipo().equals(tipo))
-    		System.out.println(this.imoveis_do_propietario.get(i).toString()); 
-    	}
+        this.imoveis_do_propietario.add(imovel);
     }
 
+    public void listaImoveis() {
+        for (int i = 0; i < this.imoveis_do_propietario.size(); i++) {
+            System.out.println(this.imoveis_do_propietario.get(i).toString());
+        }
+    }
+
+    public void listaImoveisPorTipo(String tipo) {
+        for (int i = 0; i < this.imoveis_do_propietario.size(); i++) {
+            if (this.imoveis_do_propietario.get(i).getTipo().equals(tipo))
+                System.out.println(this.imoveis_do_propietario.get(i).toString());
+        }
+    }
+
+    public String toString() {
+        return "Propietário: "+ "\nnome: " + this.nome + "\ncpf: " + this.cpf + "\nRG:" + this.rg
+                + endereco.toString();
+    }
 }
